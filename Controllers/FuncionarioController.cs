072f99b53da13649aa0166da43d37ef1c4811c49
Ismael <ismaeldev96@gmail.com>
2023-04-24 16:20:22 -0300

    Desafio Concluido

--- Controllers/FuncionarioController.cs
+++ Controllers/FuncionarioController.cs
@@ -44,13 +44,14 @@ public class FuncionarioController : ControllerBase
     public IActionResult Criar(Funcionario funcionario)
     {
         _context.Funcionarios.Add(funcionario);
+        _context.SaveChanges();
         // TODO: Chamar o método SaveChanges do _context para salvar no Banco SQL
 
         var tableClient = GetTableClient();
         var funcionarioLog = new FuncionarioLog(funcionario, TipoAcao.Inclusao, funcionario.Departamento, Guid.NewGuid().ToString());
-
+         
         // TODO: Chamar o método UpsertEntity para salvar no Azure Table
-
+        tableClient.UpsertEntity(funcionarioLog);
         return CreatedAtAction(nameof(ObterPorId), new { id = funcionario.Id }, funcionario);
     }
 
@@ -64,16 +65,22 @@ public class FuncionarioController : ControllerBase
 
         funcionarioBanco.Nome = funcionario.Nome;
         funcionarioBanco.Endereco = funcionario.Endereco;
+        funcionarioBanco.Ramal = funcionario.Ramal;
+        funcionarioBanco.EmailProfissional = funcionario.EmailProfissional;
+        funcionarioBanco.Departamento = funcionario.Departamento;
+        funcionarioBanco.Salario = funcionario.Salario;
+        funcionarioBanco.DataAdmissao = funcionario.DataAdmissao;
         // TODO: As propriedades estão incompletas
 
         // TODO: Chamar o método de Update do _context.Funcionarios para salvar no Banco SQL
+        _context.Funcionarios.Update(funcionarioBanco);
         _context.SaveChanges();
 
         var tableClient = GetTableClient();
         var funcionarioLog = new FuncionarioLog(funcionarioBanco, TipoAcao.Atualizacao, funcionarioBanco.Departamento, Guid.NewGuid().ToString());
 
         // TODO: Chamar o método UpsertEntity para salvar no Azure Table
-
+        tableClient.UpsertEntity(funcionarioLog);
         return Ok();
     }
 
@@ -86,6 +93,7 @@ public class FuncionarioController : ControllerBase
             return NotFound();
 
         // TODO: Chamar o método de Remove do _context.Funcionarios para salvar no Banco SQL
+        _context.Funcionarios.Remove(funcionarioBanco);
         _context.SaveChanges();
 
         var tableClient = GetTableClient();
